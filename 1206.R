675ebffdd6bf01df3e37d9770cc2cc7555ca80d2
LAPTOP-4PD9IIMG\user <wuyishan0805@gmail.com>
2023-12-06 17:09:06 +0800

    使用RCurl套件擷取八卦版資訊

--- 1206.R
+++ 1206.R
@@ -159,3 +159,24 @@ for (i in 1:nrow(my_table)) {
 }
 
 my_table
+
+
+##13
+URL = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+
+website = read_html(URL)
+website
+
+##八卦版有年滿十八的按鍵可叫程式幫你按
+library(RCurl)
+
+URL = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+curl = getCurlHandle()
+curlSetOpt(cookie = "over18=1", followlocation = TRUE, curl = curl)
+
+html_character = getURL(URL, curl = curl)
+
+website = read_html(html_character)
+needed_html = website %>% html_nodes("a")
+needed_txt = needed_html %>% html_text()
+needed_txt
